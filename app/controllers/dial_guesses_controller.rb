82f016b173d864384d13418527d766a3fa2eb1e3
Charlie Wilson <cwhwilson@hotmail.co.uk>
2024-03-22 10:29:20 +0000

    dial guess saved

--- app/controllers/dial_guesses_controller.rb
+++ app/controllers/dial_guesses_controller.rb
@@ -12,9 +12,11 @@ class DialGuessesController < ApplicationController
   end
 
   def create
-    @dial_guess = DialGuess.new(dial_guess_params)
     @user = current_user
     @set_dial = SetDial.find(params[:set_dial_id])
+    @dial_guess = DialGuess.new(dial_guess_params)
+    @dial_guess.user = @user
+    @dial_guess.set_dial = @set_dial
     @game = @set_dial.game
     @points = calculate_points
     @dial_guess.save
@@ -29,9 +31,6 @@ class DialGuessesController < ApplicationController
   end
 
   def calculate_points
-    @user = current_user
-    @set_dial = SetDial.find(params[:set_dial_id])
-    @dial_guess = DialGuess.new(dial_guess_params)
     @guessed_value = @dial_guess.guessed_value
     @setter_value = @set_dial.setter_value
     @guessed_value - @setter_value
