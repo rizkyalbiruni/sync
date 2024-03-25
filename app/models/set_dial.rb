c6ce55f2a96c40d606486eeaa4ff28bafceaf9fe
Charlie Wilson <cwhwilson@hotmail.co.uk>
2024-03-25 10:13:49 +0000

    results reveal

--- app/models/set_dial.rb
+++ app/models/set_dial.rb
@@ -3,4 +3,8 @@ class SetDial < ApplicationRecord
   belongs_to :user
   belongs_to :game
   has_many :dial_guesses, dependent: :destroy
+
+  def all_guessed?
+    dial_guesses.size == game.group.users.size - 1
+  end
 end
