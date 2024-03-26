9c5640b35e27f021e3633b43617471fd6ad1b90c
Charlie Wilson <cwhwilson@hotmail.co.uk>
2024-03-26 10:02:08 +0000

    results modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,5 @@ import "bootstrap"
 import "@rails/actioncable"
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+//= require jquery
