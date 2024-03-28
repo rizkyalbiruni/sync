16848e58600210b185581393dd86123eae02ecdb
Muhammad Rizky <rizky.wazza@gmail.com>
2024-03-28 17:23:41 +0100

    chat action cable fixed

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,14 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { gameId: Number }
+  static values = { gameId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
     this.subscription = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.gameIdValue },
-      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data)}
+      { received: data => this.insertMessage(data)}
     )
     console.log(`Subscribed to the chatroom with the id ${this.gameIdValue}.`)
+  }
+  insertMessage(data) {
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+  // Inserting the `message` in the DOM
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+  }
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
